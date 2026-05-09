e2783259805058e56f9305e803fe2fb2b6f87dbf
developerthierry <developerthierry@gmail.com>
2026-05-09 08:40:01 +0000

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+const port = 8000
+const logger = (req, res, next) => {
+    console.log('Middleware: '+ req.method + "  "+ req.url)
+    next()
+}
+app.use(express.json())
+app.use(logger)
+app.get("/", (req, res) => {
+    res.send('Hello from Express Sever')
+})
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`))
